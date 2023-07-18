4a7f96b1cfaa88624ccc1788d0313c0d851e1911
INSHAKE <inseok5053@gmail.com>
2023-07-18 21:20:53 +0900

    [test4] import / export / map() / props / component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav, Navbar, Row, Col} from "react-bootstrap";
+import productData from "./data";
+import { useState } from "react";
+
+// @@@ 서버에서 가져왔다고 치고 여긴 서버임
+
+// @@@ 서버에서 가져왔다고 치고 여긴 서버임
 
 function App() {
+
+  const [Data] = useState(productData)
+
   return (
-    <div className="AppTest">
-      기능개발 기능개발
+    <div className="App">
+      <>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">NoMall</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="detail">상세페이지</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+
+      <div className="main-bg"></div>
+
+      <ProductList Data = {Data}/>
+
     </div>
   );
+}
+
+function ProductList(props) {
+  return (
+    <Container>
+      <Row>
+        {props.Data.map((item) => (
+          <Col key={item.id} xs={6} md={4}>
+            <img src={`https://codingapple1.github.io/shop/shoes${item.id + 1}.jpg`} width="80%" alt=""/>
+            <h4>{props.Data[item.id].title}</h4>
+            <p>{props.Data[item.id].content}</p>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+let productData = [
+  {
+    id: 0,
+    title: "White and Black",
+    content: "Made in China",
+    price: 120000,
+  },
+
+  {
+    id: 1,
+    title: "Red Knit",
+    content: "Made in Korea",
+    price: 110000,
+  },
+
+  {
+    id: 2,
+    title: "Grey Yordan",
+    content: "Made in Indonesia",
+    price: 130000,
+  },
+];
+
+export default productData;
