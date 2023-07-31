31ae80adbe4d899fabeb5a4b6f3648391392d18f
INSHAKE <inseok5053@gmail.com>
2023-07-31 19:47:41 +0900

    styled.components / url detail.id에 따라 page 보여주기 /

--- src/pages/AboutPages.js
+++ src/pages/AboutPages.js
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
+import styled from 'styled-components'
+
+let Ybtn = styled.button`
+  background ; yellow;
+  color = white;
+  padding : 10px;
+  pont-size : 20px;
+`
 
 function AboutPages() {
   return (
   <div>
     <div>회사 정보</div>
     <Outlet></Outlet>
+    <Ybtn>Button</Ybtn>
   </div>
   )
 }
--- src/pages/DetailPages.js
+++ src/pages/DetailPages.js
@@ -1,19 +1,29 @@
 import { useParams } from "react-router-dom";
+import styled from 'styled-components'
 
+let Btn = styled.button`
+  background: ${ props => props.bg};
+  color: ${ props => props.bg === 'blue' ? 'white' : ''};
+  padding: 10px;
+  font-size: 20px;
+`
 function DetailPages(props) {
 
-  let {id}= useParams()
+  let { id } = useParams();
+  let pagenum = props.Data.find((x) => x.id === parseInt(id));
   
   return(
     <div className="container">
+      <Btn bg='blue'>Btn</Btn>
+      <Btn bg='orange'>Btn</Btn>
       <div className="row">
         <div className="col-md-6">
-          <img src={`https://codingapple1.github.io/shop/shoes${props.Data[id].id + 1}.jpg`} width="100%" alt=""/>
+          <img src={`https://codingapple1.github.io/shop/shoes${pagenum.id + 1}.jpg`} width="100%" alt=""/>
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.Data[id].title}</h4>
-          <p>{props.Data[id].content}</p>
-          <p>{props.Data[id].price}원</p>
+          <h4 className="pt-5">{pagenum.title}</h4>
+          <p>{pagenum.content}</p>
+          <p>{pagenum.price}원</p>
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
